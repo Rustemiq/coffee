21d83f21fbf0d705220293dc244ec8ea5ae21c26
Rustem Kharisov <rustem.renatovich@gmail.com>
2024-12-07 17:59:15 +0500

    Добавлен редактор БД

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import sqlite3
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QWidget
 from PyQt6 import uic
 
 
@@ -10,6 +10,7 @@ class MyWidget(QMainWindow):
         super().__init__()
         uic.loadUi('main.ui', self)
         self.pushButton.clicked.connect(self.loadTable)
+        self.addButton.clicked.connect(self.openEditor)
 
     def loadTable(self):
         con = sqlite3.connect('coffee.sqlite')
@@ -28,6 +29,24 @@ class MyWidget(QMainWindow):
             ['id', 'название', 'обжарка', 'молотый?', 'вкус', 'цена (руб)', 'вес (гр)'])
         self.tableWidget.resizeColumnsToContents()
 
+    def openEditor(self):
+        self.editorForm = EditorForm()
+        self.editorForm.show()
+
+class EditorForm(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.con = sqlite3.connect('coffee.sqlite')
+        self.cur = self.con.cursor()
+        self.okButton.clicked.connect(self.addData)
+
+    def addData(self):
+        try:
+            self.cur.execute(self.request.toPlainText())
+            self.con.commit()
+        except Exception as e:
+            print(e)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
